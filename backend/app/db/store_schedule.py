2ebeb7c6583419856842730d4355d8bce2b40ee1
Kulunu Abeysinghe <kabe0009@student.monash.edu>
2026-01-21 19:33:20 +1100

    fix incorrect date parse and invalid team_id errors

--- backend/app/db/store_schedule.py
+++ backend/app/db/store_schedule.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from nba_api.stats.endpoints import scheduleleaguev2
 from datetime import datetime
 from app.models.game_schedule import GameSchedule
+from app.models.team import Team
 
 
 async def load_schedule(db: AsyncSession, season: str):
@@ -13,10 +14,22 @@ async def load_schedule(db: AsyncSession, season: str):
     sched = scheduleleaguev2.ScheduleLeagueV2(season=season)
     df_games = sched.season_games.get_data_frame()
 
+    # Get all team IDs in DB (to check if team is nba team before insert)
+    result = await db.execute(select(Team.id))
+    team_ids_in_db = {row[0] for row in result.fetchall()}
+
     inserted = 0
     skipped = 0
 
     for _, row in df_games.iterrows():
+        home_id = row["homeTeam_teamId"]
+        away_id = row["awayTeam_teamId"]
+
+        # skip if either team is not in DB
+        if home_id not in team_ids_in_db or away_id not in team_ids_in_db:
+            skipped += 1
+            continue
+
         game_id = row["gameId"]
 
         # skip if already exists
@@ -28,8 +41,8 @@ async def load_schedule(db: AsyncSession, season: str):
             continue
 
         game_date = (
-            datetime.strptime(row["gameDate"], "%b %d, %Y").date()
-            if "gameDate" in row
+            datetime.strptime(row["gameDate"], "%m/%d/%Y %H:%M:%S").date()
+            if "gameDate" in row and row["gameDate"]
             else datetime.strptime(row["gameDateEst"], "%Y-%m-%d").date()
         )
 
@@ -39,8 +52,8 @@ async def load_schedule(db: AsyncSession, season: str):
                 game_date=game_date,
                 season=row["seasonYear"],
                 season_type=row.get("gameSubtype", "Regular Season"),
-                home_team_id=row["homeTeam_teamId"],
-                away_team_id=row["awayTeam_teamId"],
+                home_team_id=home_id,
+                away_team_id=away_id,
                 home_team_abbr=row["homeTeam_teamTricode"],
                 away_team_abbr=row["awayTeam_teamTricode"],
                 matchup=f"{row['awayTeam_teamTricode']} @ {row['homeTeam_teamTricode']}",
